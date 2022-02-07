fd07d16b7bf4a979286c4ca49e5d05f41103d766
Muhammad Mohsin Khan <77793691+mohsinkhansdev@users.noreply.github.com>
2022-02-07 19:11:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 
 
-
 var colour = ["red","blue","green"];
 
 colour
@@ -16,4 +15,22 @@ colour
 
 colour.push("oragne");
 colour.pop("pink");
+
+// JavaScript Object Practice
+var dog = {
+    name:"rusty",
+    age: 21,
+    city: "US" 
+}
+
+dog.age
+
+var person = { 
+name: "Travis", 
+age: 21,
+city: "london"}
+
+person ["name"] = "Tater";
+person.name
+'Tater'
 
