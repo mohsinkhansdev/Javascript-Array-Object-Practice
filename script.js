e3d3a02cc6821d21ede04813539c9d20c4d8b1f8
Muhammad Mohsin Khan <77793691+mohsinkhansdev@users.noreply.github.com>
2022-02-07 18:59:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// JavaScript Functions Practice
+
+
+
+
+var colour = ["red","blue","green"];
+
+colour
+(3) ['red', 'blue', 'green']
+
+colour [3] = "pink";
+'pink'
+
+colour
+(4) ['red', 'blue', 'green', 'pink']
+
+colour.push("oragne");
+colour.pop("pink");
+
